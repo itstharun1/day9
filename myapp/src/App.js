7c74cdf2489ceaa8f2d65a80e5bc972078945e2d
itstharun1 <tharunganjai@gmail.com>
2024-03-20 12:24:22 +0530

    day9

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import Home from "./components/Home"
+import Nav from "./components/Nav"
+
+const App=()=>{
+  return(
+    <div>
+    <Nav/>
+    </div>
+  )
+}
+export default App
---
+++ myapp/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Nav=()=>{
+    return(
+        <div className="nav">
+            <h1 className='item'>Home</h1>
+            <h1 className='item'>About</h1>
+            <h1 className='item'>Contact</h1>
+        </div>
+    )
+}
+
+export default Nav
